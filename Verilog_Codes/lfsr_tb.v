ff7fc43bcc74b14a71f9569c7aa4084926dceb26
Bala Krishna Sai <brucewayne25062000@gmail.com>
2026-04-06 08:58:02 +0530

    Update lfsr_tb.v

--- Verilog_Codes/lfsr_tb.v
+++ Verilog_Codes/lfsr_tb.v
@@ -23,3 +23,25 @@ module lfsr_tb;
   
   initial $monitor("feedback = %b, out = %b", uut.feedback, q);
 endmodule
+
+
+/* OUTPUT:
+
+# KERNEL: feedback = 0, out = 1111
+# KERNEL: feedback = 1, out = 0111
+# KERNEL: feedback = 0, out = 1011
+# KERNEL: feedback = 1, out = 0101
+# KERNEL: feedback = 1, out = 1010
+# KERNEL: feedback = 0, out = 1101
+# KERNEL: feedback = 0, out = 0110
+# KERNEL: feedback = 1, out = 0011
+# KERNEL: feedback = 0, out = 1001
+# KERNEL: feedback = 0, out = 0100
+# KERNEL: feedback = 0, out = 0010
+# KERNEL: feedback = 1, out = 0001
+# KERNEL: feedback = 1, out = 1000
+# KERNEL: feedback = 1, out = 1100
+# KERNEL: feedback = 1, out = 1110
+# KERNEL: feedback = 0, out = 1111
+
+*/
