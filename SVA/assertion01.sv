23d8ffb6bdb56ba8dc3f601ef404cc6fc02a355f
Bala Krishna Sai <brucewayne25062000@gmail.com>
2026-04-10 13:07:33 +0530

    Update assertion01.sv

--- SVA/assertion01.sv
+++ SVA/assertion01.sv
@@ -40,13 +40,13 @@ endmodule
 
 
 /* OUTPUT:
-  V e r i l a t i o n   R e p o r t: Verilator 5.044 2026-01-01 rev v5.044
-- Verilator: Built from 0.042 MB sources in 2 modules, into 0.033 MB in 9 C++ files needing 0.000 MB
-- Verilator: Walltime 9.225 s (elab=0.001, cvt=0.006, bld=9.209); cpu 0.016 s on 8 threads; alloced 30.277 MB
-[5] PASS (vacuous): enable did not rise
-[15] FAIL: enable rose while reset high
-[25] PASS (vacuous): enable did not rise
-[35] PASS (real): enable rose and reset was low
-- testbench.sv:36: Verilog $finish
-- S i m u l a t i o n   R e p o r t: Verilator 5.044 2026-01-01
+# Loading sv_std.std
+# Loading work.assertion1(fast)
+# run -all
+# [15] FAIL: enable rose while reset high
+# [35] PASS (real): enable rose and reset was low
+# ** Note: $finish    : assertion1.sv(36)
+#    Time: 40 ns  Iteration: 0  Instance: /assertion1
+# End time: 13:06:57 on Apr 10,2026, Elapsed time: 0:00:01
+# Errors: 0, Warnings: 0
 */
