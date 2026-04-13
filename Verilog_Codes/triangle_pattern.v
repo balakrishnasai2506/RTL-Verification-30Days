7e07b728cdbae5a302943d2a25ede87703a2d1c1
Bala Krishna Sai <brucewayne25062000@gmail.com>
2026-04-13 17:24:43 +0530

    Create triangle_pattern.v

---
+++ Verilog_Codes/triangle_pattern.v
@@ -0,0 +1,33 @@
+/* Generate the pattern:
+1
+22
+333
+4444
+*/
+module pattern;
+  integer n;
+  int arr[];
+  
+  integer i, j;
+  
+  initial begin
+    n = 6;
+    arr = new[n];
+    for(i = 0; i < n; i = i+1) begin
+      for(j = 0; j <= i; j = j+1)
+        $write("%0d", i+1);
+      $display("");
+    end
+  end
+endmodule
+
+
+/* OUTPUT:
+# KERNEL: 1
+# KERNEL: 22
+# KERNEL: 333
+# KERNEL: 4444
+# KERNEL: 55555
+# KERNEL: 666666
+# KERNEL: Simulation has finished. There are no more test vectors to simulate.
+*/
