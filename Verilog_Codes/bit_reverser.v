7aeaf75e79a340591b01fc25a0ad5e418117f27f
Bala Krishna Sai <brucewayne25062000@gmail.com>
2026-04-13 17:12:44 +0530

    Create bit_reverser.v

---
+++ Verilog_Codes/bit_reverser.v
@@ -0,0 +1,9 @@
+// Write code to reverse bits of an 8-bit number.
+
+module bit_reverser(input [7:0] in, output reg [7:0] out);
+  integer i;
+  always @(*) begin
+    for(i = 0; i < 8; i = i + 1)
+      out[i] = in[7 - i];
+  end
+endmodule
